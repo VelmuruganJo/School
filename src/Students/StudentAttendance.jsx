ba169e959d1b0c50b3f9eabdfed937d4beec0278
VelmuruganJo <velvm1997@gmail.com>
2025-07-31 16:20:06 +0530

    m

--- src/Students/StudentAttendance.jsx
+++ src/Students/StudentAttendance.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './Sytle/SchoolStudent.css'
+import './Style/SchoolStudent.css'
 
 function StudentAttendance() {
   const navigate = useNavigate();
